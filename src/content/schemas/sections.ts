135ee21fa59f2ad2482e66f6727a97500b42c57d
Louis <louis.morgan@linaro.org>
2024-04-09 15:55:42 +0100

    implement consulting page

--- src/content/schemas/sections.ts
+++ src/content/schemas/sections.ts
@@ -140,6 +140,16 @@ export const logoGridSchema = z.object({
   ),
 });
 
+export const graphicSchema = z.object({
+  component: component("graphic"),
+  src: z.string(),
+  mobile_src: z.string().optional(),
+  alt: z.string(),
+  accessible_version: z.string().optional(),
+  width: z.number(),
+  height: z.number(),
+});
+
 export default z.discriminatedUnion("component", [
   buttonsSchema,
   fileCarouselSchema,
@@ -152,4 +162,5 @@ export default z.discriminatedUnion("component", [
   backcardsSchema,
   moreInfoSchema,
   logoGridSchema,
+  graphicSchema,
 ]);
