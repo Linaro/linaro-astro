582f860bf503b67145dd5ed548bd75a1ccae1801
Louis <louis.morgan@linaro.org>
2024-05-22 11:08:20 +0100

    fix astro config conflict

--- astro.config.mjs
+++ astro.config.mjs
@@ -83,6 +83,7 @@ export default defineConfig({
     rollupOptions: {
       external: ["/pagefind/pagefind.js"],
     },
+    redirects: true,
   },
   vite: {
     optimizeDeps: { exclude: ["auth:config"] },
