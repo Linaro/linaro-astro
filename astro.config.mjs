5efb189d6e257a874cc13f6c013f45b2d4d825a4
Philip Colmer <philip.colmer@linaro.org>
2024-06-04 15:39:53 +0100

    Restore requested blog and redirect whitepaper link

--- astro.config.mjs
+++ astro.config.mjs
@@ -105,5 +105,6 @@ export default defineConfig({
     '/about/team': '/about',
     '/about/tsc': '/about',
     // '/learning-hub/[...slug]': '/about',
+    '/whitepapers/protecting-investment-in-code-optimisation-with-toolchain-ci-linaro-white-paper/': '/whitepapers',
   }
 });
