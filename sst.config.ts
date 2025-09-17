87201ce13d5abd5e1abad787be5cab4d83fca0c2
Emmanuel Olaotan <80754155+kenzo7664@users.noreply.github.com>
2025-09-17 11:05:54 +0100

    Add CLOUDINARY_UNSIGNED_UPLOAD_PRESET to sst config

--- sst.config.ts
+++ sst.config.ts
@@ -22,7 +22,10 @@ export default {
             CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY!,
             CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET!,
             CLOUDINARY_URL: process.env.CLOUDINARY_URL!,
-            PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.PUBLIC_CLOUDINARY_CLOUD_NAME!,
+            CLOUDINARY_UNSIGNED_UPLOAD_PRESET:
+              process.env.CLOUDINARY_UNSIGNED_UPLOAD_PRESET!,
+            PUBLIC_CLOUDINARY_CLOUD_NAME:
+              process.env.PUBLIC_CLOUDINARY_CLOUD_NAME!,
           },
         });
         stack.addOutputs({
@@ -51,7 +54,10 @@ export default {
             CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY!,
             CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET!,
             CLOUDINARY_URL: process.env.CLOUDINARY_URL!,
-            PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.PUBLIC_CLOUDINARY_CLOUD_NAME!,
+            CLOUDINARY_UNSIGNED_UPLOAD_PRESET:
+              process.env.CLOUDINARY_UNSIGNED_UPLOAD_PRESET!,
+            PUBLIC_CLOUDINARY_CLOUD_NAME:
+              process.env.PUBLIC_CLOUDINARY_CLOUD_NAME!,
             IS_PREVIEW: process.env.IS_PREVIEW!,
           },
         });
