81fbc272af335cec6939d98801f2cfc3277ac1e9
Philip Colmer <philip.colmer@linaro.org>
2026-01-29 13:31:29 +0000

    FIx missing Lambda permission

--- sst.config.ts
+++ sst.config.ts
@@ -45,6 +45,17 @@ export default $config({
       domain,
     });
 
+    // This is *TEMPORARILY* needed because of a bug between SST v3
+    // and Pulumi, missing out an additional permission required by AWS.
+    if (site?.nodes?.server) {
+      new aws.lambda.Permission("MyServiceInvokePermission", {
+        action: "lambda:InvokeFunction",
+        function: site.nodes.server.name,
+        principal: "*",
+        statementId: "FunctionURLInvokeAllowPublicAccess",
+      });
+    }
+
     return {
       url: site.url,
     };
